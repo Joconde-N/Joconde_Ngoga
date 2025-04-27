e5e4848ea871825166dc576aa81914c88f26c018
Ngoga Joconde <jocpinky04@gmail.com>
2025-04-27 21:30:18 +0200

    Add files via upload
    Traffic fine management system files

--- Main.java
+++ Main.java
@@ -1,77 +1,66 @@
-package Assignment_one.Question2;
+package Assignment_one.Question3;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        System.out.println("Welcome to the LEMIGO HOTEL management system");
+        System.out.println("--Welcome to the Rwanda National Police Traffic Management System--");
 
-        String guestId="";
-        while (true) {
-            System.out.print("Enter guest ID: ");
-            guestId = in.nextLine();
-            if (guestId.matches("\\d+")) {
-                break;
-            } else {
-                System.out.println("Error: Guest ID must contain digits only.");
-            }
-        }
-        
-        String guestName="";
-        while (true) {
-            System.out.print("Enter guest Name: ");
-            guestName = in.nextLine();
-            if (guestName.matches("[a-zA-Z ]+")) {
-                break;
-            } else {
-                System.out.println("Error: Guest name must contain only letters.Try again");
-                
-            }
-        }
-        System.out.println("Enter room type (STANDARD, DELUXE or SUITE): ");
-        String roomType=in.nextLine();
-        if(!roomType.equals("STANDARD")&&!roomType.equals("DELUXE")&&!roomType.equals("SUITE")){
-            System.out.println("Error: invalid room type! Please enter STANDARD, DELUXE or SUITE");
-            roomType=in.nextLine();
-        }
-        int stayDays = 0; 
-        while (true) {
-            try{
-        System.out.println("Enter number of days you want to stay between 1 and 30: ");
-        stayDays=in.nextInt(); 
-        if(stayDays<1||stayDays>30){
-            System.out.println("Error: invalid number of days! Please enter a number between 1 and 30");
-        } else {
+        String driverId="";
+        while(true){
+        System.out.println("Enter the driver ID (must be 16 digits): ");
+        driverId=in.nextLine();
+        if (driverId.matches("\\d{16}")) {
             break; 
+        } else {
+            System.out.println("Error: The ID must contain 16 digits and only numbers. Please try again.");
         }
-        }catch(Exception e){
-            System.out.println("Error: invalid input! please enter a numeric value");
-            in.nextLine(); 
+    }
+
+
+        String driverName="";
+        while (true) {
+        System.out.println("Enter the driver's name: ");
+        driverName=in.nextLine();
+        if (driverName.matches("[a-zA-Z ]+")) {
+            break;
+        } else {
+            System.out.println("Error: driver's name must contain only letters.Try again");
+            
         }
+    }
+
+        System.out.println("Enter the vehicle plate (e.g., RAB123D): ");
+        String vehiclePlate=in.nextLine();
+        while (!Pattern.matches("[R][A-Z]{2}\\d{3}[A-Z]", vehiclePlate)) {
+            System.out.print("Invalid plate format. Try again (e.g., RAB123D): ");
+            vehiclePlate = in.nextLine();
+        }
+        System.out.println("Enter Violation Type (SPEEDING, RED_LIGHT, NO_HELMET or DUI): ");
+        String violationType = in.nextLine();
+        if(!violationType.equals("SPEEDING")&&!violationType.equals("RED_LIGHT")&&!violationType.equals("NO_HELMET")&&!violationType.equals("DUI")){
+            System.out.println("Error: invalid violation type! Please enter SPEEDING, RED_LIGHT, NO_HELMET or DUI ");
+            violationType=in.nextLine();
         }
 
-        in.nextLine();
-        String roomStatus="AVAILABLE";
-
-        RoomBooking room=new RoomBooking(guestId, guestName, roomType, stayDays, roomStatus);
-        room.bookRoom();
-        roomStatus = "OCCUPIED";
-
-        System.out.println("Do you want to checkout?(yes/no): ");
+        ViolationEntry vio=new ViolationEntry(driverId, driverName, vehiclePlate, violationType, 0, violationType);
+        vio.recordViolation();
+        System.out.println("");
+        FineAssessment fine=new FineAssessment(driverId, driverName, vehiclePlate, violationType, 0, violationType);
+        fine.assessFine();
+        System.out.println("Do you want to pay the fine now (yes/no)?: ");
         String choice=in.nextLine();
         if(choice.equalsIgnoreCase("yes")){
-            GuestCheckout gue=new GuestCheckout(guestId, guestName, roomType, stayDays, roomStatus);
-            gue.checkoutGuest();
-
-            Billing bi=new Billing(guestId, guestName, roomType, stayDays, roomStatus);
-            bi.generateBill();
+            FinePayment pay=new FinePayment(driverId, driverName, vehiclePlate, violationType, 0, choice);
+            pay.processPayment();
+        
         }else{
-            System.out.println(" Enjoy your stay :| ");
+            System.out.println("Fine is not yet paid. Please pay it sooner!");
+
         }
         in.close();
+
+        
     }
-       
-
-
-    
     
 }
